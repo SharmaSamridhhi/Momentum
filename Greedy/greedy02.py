e723832643724fe984ef15551be3183cb010af7a
Samridhhi Sharma <sharma.samridhhi9@gmail.com>
2026-08-30 20:11:46 +0530

    Day 01: solved Lemonade Change

---
+++ Greedy/greedy02.py
@@ -0,0 +1,25 @@
+def lemonadeChange(bills):
+    five, ten = 0,0
+
+    for i in range(len(bills)):
+        if bills[i] == 5:
+            five += 1
+            
+        elif bills[i] == 10:
+            if (five):
+                    five -= 1
+                    ten += 1
+            else:
+                return False
+            
+        else:
+            if (ten and five):
+                ten-=1
+                five-=1
+            elif five >= 3:
+                five-=3
+
+            else:
+                return False
+        
+    return True
